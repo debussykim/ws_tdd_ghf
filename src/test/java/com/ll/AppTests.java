b81d91513479233d85ff5de2ddb20c03427a8939
JH <118197952+debussykim@users.noreply.github.com>
2023-03-06 16:32:38 +0900

    Enhancement/1 (#2)
    * 테스트용
    
    * test: 출력을 모니터에 하지 않고 문자열로 얻기
    
    * feat: 종료 기능 구현, 잘못 입력된 명령어 처리
    
    * feat: AppTestRunner 도입해서 중복제거
    
    * test: 명언 등록할 때 마다 번호 증가

--- src/test/java/com/ll/AppTests.java
+++ src/test/java/com/ll/AppTests.java
@@ -1,5 +1,100 @@
 package com.ll;
 
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AppTests {
+    // TestUtil Test Start
+    @Test
+    @DisplayName("스캐너에 키보드가 아닌 문자열을 입력으로 설정")
+    public void t1() {
+        Scanner sc = TestUtil.getScanner("Hello World");
+
+        String cmd = sc.nextLine().trim();
+        assertThat(cmd).isEqualTo("Hello World");
+    }
+
+    @Test
+    @DisplayName("출력을 모니터에 하지 않고 문자열로 얻기")
+    public void t2() {
+        // System.out 에 대한 화면출력 금지 시작
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        System.out.print("안녕");
+
+        // 그 동안 출력되지 않던 내용들을 문자열로 반환
+        String rs = output.toString();
+
+        // System.out 에 대한 화면출력 금지 끝
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertThat(rs).isEqualTo("안녕");
+    }
+    // TestUtil Test End
+
+    // App Test Start
+    @Test
+    @DisplayName("프로그램 시작 시 타이틀 출력 그리고 종료")
+    public void t3() {
+        String rs = AppTestRunner.run("");
+
+        assertThat(rs)
+                .contains("== 명언 앱 ==")
+                .contains("명령) ")
+                .contains("프로그램이 종료되었습니다.")
+                .doesNotContain("올바르지 않은 명령입니다.");
+    }
+
+    @Test
+    @DisplayName("잘못된 명령어 입력에 대한 처리")
+    public void t4() {
+        String rs = AppTestRunner.run("종료2");
+
+        assertThat(rs)
+                .contains("올바르지 않은 명령입니다.");
+    }
+
+    @Test
+    @DisplayName("등록화면에서 명언과 작가를 입력받고 명언 생성")
+    public void t5() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(rs)
+                .contains("명언 : ")
+                .contains("작가 : ")
+                .contains("1번 명언이 등록되었습니다.");
+    }
+
+    @Test
+    @DisplayName("명언이 등록될 때 마다 생성되는 명언의 번호가 1씩 증가한다.")
+    public void t6() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                나의 죽음을 적들에게 알리지 마라.
+                이순신
+                등록
+                왼손은 거들뿐
+                강백호
+                """);
+
+        assertThat(rs)
+                .contains("1번 명언이 등록되었습니다.")
+                .contains("2번 명언이 등록되었습니다.")
+                .contains("3번 명언이 등록되었습니다.")
+                .doesNotContain("4번 명언이 등록되었습니다.");
+    }
+    // App Test End
 }

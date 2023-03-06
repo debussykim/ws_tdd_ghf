ad1bddef61a2519ce31d3bb4ca9b2a39b0c318cd
debussysuh <debussysuh@gmail.com>
2023-03-06 14:18:02 +0900

    테스트용

--- src/test/java/com/ll/AppTests.java
+++ src/test/java/com/ll/AppTests.java
@@ -1,5 +1,20 @@
 package com.ll;
 
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AppTests {
+    @Test
+    @DisplayName("스캐너에 키보드가 아닌 문자열을 입력으로 설정")
+    public void t1() {
+        Scanner sc = TestUtill.getScanner("Hello World");
+
+        String cmd = sc.nextLine().trim();
+        assertThat(cmd).isEqualTo("Hello World");
+    }
 }
--- src/test/java/com/ll/TestUtill.java
+++ src/test/java/com/ll/TestUtill.java
@@ -1,4 +1,13 @@
 package com.ll;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class TestUtill {
+    public static Scanner getScanner(String input) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+
+        return new Scanner(in);
+    }
 }

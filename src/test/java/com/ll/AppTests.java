e511fb71e0291423e3844bf1edc4ffd48f152b49
debussysuh <debussysuh@gmail.com>
2023-03-06 15:44:18 +0900

    feat: 종료 기능 구현, 잘못 입력된 명령어 처리

--- src/test/java/com/ll/AppTests.java
+++ src/test/java/com/ll/AppTests.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTests {
+    // TestUtil Test Start
     @Test
     @DisplayName("스캐너에 키보드가 아닌 문자열을 입력으로 설정")
     public void t1() {
@@ -35,4 +36,42 @@ public class AppTests {
 
         assertThat(rs).isEqualTo("안녕");
     }
+    // TestUtil Test End
+
+    // App Test Start
+    @Test
+    @DisplayName("프로그램 시작 시 타이틀 출력 그리고 종료")
+    public void t3() {
+        Scanner sc = TestUtil.getScanner("종료");
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        new App(sc).run();
+
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertThat(rs)
+                .contains("== 명언 앱 ==")
+                .contains("명령) ");
+
+    }
+
+    @Test
+    @DisplayName("잘못된 명령어 입력에 대한 처리")
+    public void t4() {
+        Scanner sc = TestUtil.getScanner("""
+                종료2
+                종료
+                """.stripIndent().trim());
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        new App(sc).run();
+
+        String rs = output.toString();
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertThat(rs)
+                .contains("올바르지 않은 명령입니다.");
+    }
+    // App Test End
 }
